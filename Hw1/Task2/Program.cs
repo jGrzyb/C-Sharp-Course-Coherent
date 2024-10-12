2fb693f3b9bd6c793253c43c68358a76a59dffac
jGrzyb <grzybjaku@gmail.com>
2024-10-12 22:41:28 +0200

    Hw1 v0.1

---
+++ Hw1/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Put your nine digit code: ");
+string input = Console.ReadLine() ?? "000000000";
+int sum = 0;
+for(int i=0; i<input.Length; i++) {
+    sum += (i+1) * (input[i] - '0');
+}
+string result = "";
+if(sum % 11 == 0) {
+    result = input + '0';
+}
+else if(sum % 11 == 1) {
+    result = input + 'X';
+}
+else {
+    result = input + (11 - sum % 11);
+}
+Console.WriteLine(result);
---
+++ Hw1/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Put number of elements: ");
+int count = int.Parse(Console.ReadLine() ?? "1");
+int[] array = new int[count];
+for(int i=0; i<count; i++) {
+    Console.Write("Put your " + (i+1) + " value: ");
+    array[i] = int.Parse(Console.ReadLine() ?? "1");
+}
+
+for(int i=0; i<array.Length; i++) {
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int[] bufforArray = new int[count];
+int bufforSize = 0;
+for(int i=0; i<count; i++) {
+    bool isAlreadyIn = false;
+    for(int j=0; j<bufforSize; j++) {
+        if(bufforArray[j] == array[i]) {
+            isAlreadyIn= true;
+        }
+    }
+    if(!isAlreadyIn) {
+        bufforArray[bufforSize] = array[i];
+        bufforSize++;
+    }
+}
+int[] finalArray = new int[bufforSize];
+for(int i=0; i<bufforSize; i++) {
+    finalArray[i] = bufforArray[i];
+}
+
+for(int i=0; i<finalArray.Length; i++) {
+    Console.Write(finalArray[i] + " ");
+}
+Console.WriteLine();
