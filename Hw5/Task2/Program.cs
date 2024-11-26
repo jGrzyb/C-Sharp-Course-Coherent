02a742a66c0efcd86ab5e780c8bc46b062eec09c
jGrzyb <grzybjaku@gmail.com>
2024-11-26 14:58:44 +0100

    repair again

--- Hw5/Task2/Program.cs
+++ Hw5/Task2/Program.cs
@@ -1,19 +1,6 @@
 ﻿Catalog catalog = new();
-Book[] books = [
-    new("at", ["aa", "ca", "ba"], new DateTime(2000, 1, 1)),
-    new("bt", ["aa", "da", "ba"], new DateTime(2000, 2, 1)),
-    new("ft", ["aa", "ca", "da"], new DateTime(2000, 1, 2)),
-    new("dt", ["aa", "ca"], new DateTime(2000, 4, 3)),
-    new("et", ["aa"], new DateTime(2000, 5, 4)),
-    new("ct", ["da", "ca", "ba"], new DateTime(2000, 1, 7))
-];
-
-for(int i=0; i<books.Length; i++)
-{
-    catalog.Add((i % 2 == 0 ? "123456789012" : "123-4-56-789012-")+i, books[i]);
-}
-catalog.GetTitlesAlphabetical().ToList().ForEach(Console.WriteLine);
-Console.WriteLine();
-catalog.GetBooksByAuthor("aa").ToList().ForEach(x => Console.WriteLine(x.Title));
-Console.WriteLine();
-catalog.GetAuthorsWithBookCount().ToList().ForEach(x => Console.WriteLine(x.Item1 + " " + x.Item2));
+catalog.Add("9780515110234", new Book("The Floating Admiral", ["The Detection Club", "G.K. Chesterton", "Victor L. Whitechurch"], new DateTime(1990, 5, 10)));
+catalog.Add("9780743285452", new Book("The Sword of Shame", ["The medival Murders", "Philip Gooden", "Susanna Gregory", "Michael Jecks", "Brenard Knight"], new DateTime(2006, 1, 1)));
+catalog.Add("9780060763442", new Book("The Last Templar", ["Michael Jecks"], new DateTime(1995, 1, 1)));
+catalog.Add("9780060763466", new Book("The Merchant's Partner", ["Michael Jecks"], new DateTime(1996, 6, 13)));
+catalog.Add("9781569475126", new Book("The Salisbury Manuscript", ["Philip Gooden"], new DateTime(2008, 1, 1)));
