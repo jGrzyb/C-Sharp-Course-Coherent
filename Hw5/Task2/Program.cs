69546d0030003bb916ff139365d31ec84afc887a
jGrzyb <grzybjaku@gmail.com>
2024-12-30 23:10:26 +0100

    ebook if no isbn

--- Hw5/Task2/Program.cs
+++ Hw5/Task2/Program.cs
@@ -23,3 +23,6 @@ Library paperLibrary = builder.Build("books_info.csv", "PaperLibrary");
 File.AppendAllText("output.txt", "\n\n\n" + paperLibrary.ToString());
 Directory.SetCurrentDirectory("pLib");
 repository.Save(paperLibrary.catalog);
+
+Console.WriteLine($"{eLibrary.catalog.dictionary.Count} books in eLibrary");
+Console.WriteLine($"{paperLibrary.catalog.dictionary.Count} books in paperLibrary");
